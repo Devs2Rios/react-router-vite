97d98c3b543efc615905da28ef6c86776ea42fb8
eduairet <hola@eduairet.com>
2023-07-21 23:31:04 -0600

    Error pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,13 @@ import Layout from "./components/Layout";
 const router = createBrowserRouter([
   {
     path: "/", element: <Layout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/", element: <Home />,
       },
       {
         path: "/artists", element: <Artists />,
-      },
-      {
-        path: "*", element: <NotFound />,
       },
     ]
   },
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,12 @@
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
 export default function NotFound(): JSX.Element {
     return (
-        <h1>Not Found</h1>
+        <>
+            <Nav />
+            <h1>Not Found</h1>
+            <Footer />
+        </>
     );
 }
